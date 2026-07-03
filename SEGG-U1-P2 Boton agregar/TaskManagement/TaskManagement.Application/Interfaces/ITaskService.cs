395bd5a83846967a55bcc02a881aee5b93cfdee8
Valeria <valecorts6@gmail.com>
2026-07-02 23:24:29 -0600

    VulnerableApp y Práctica 3 Unidad 2

--- SEGG-U1-P2 Boton agregar/TaskManagement/TaskManagement.Application/Interfaces/ITaskService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TaskManagement.Application.DTOs;
-namespace TaskManagement.Application.Interfaces;
-
-public interface ITaskService
-{
-    Task<IEnumerable<TaskDto>> GetTasksAsync();
-    Task<TaskDto> CreateTaskAsync(CreateTaskRequest request);
-    Task CompleteTaskAsync(Guid id);
-}
